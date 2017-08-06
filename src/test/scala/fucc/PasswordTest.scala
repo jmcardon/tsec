c2e6ed2ec7321b38d191c5f59a3ce0f0103e9e44
Robert Soeldner <r.soeldner@gmail.com>
2017-08-06 10:48:06 +0200

    Add few bcrypt tests

--- src/test/scala/fucc/PasswordTest.scala
+++ src/test/scala/fucc/PasswordTest.scala
@@ -1,5 +1,44 @@
-package fucc
+import cats.Eq
+import fucc.messagedigests.javahasher._
+import fucc.messagedigests.javahasher.syntax._
+import fucc.passwordhashers.core._
+import fucc.passwordhashers.syntax._
+import fucc.passwordhashers.instances._
+import cats.syntax.either._
+import fucc.passwordhashers.instances.HardenedSCrypt.SCryptPasswordHasher
+import fucc.passwordhashers.instances.SCrypt.SCryptPasswordHasher
 
-class PasswordTest {
+class PasswordTest extends TestSpec {
+
+  val plainPassword = "abc"
+
+  /**
+    * Bcrypt
+    */
+
+  "BCrypt password hasher" should "generate and verify with default settings" in {
+    implicit val bcrypt = BCryptPasswordHasher()
+    val hash: PasswordValidated[BCrypt] = plainPassword.hash
+
+    assert(hash.flatMap(bcrypt.checkHashed(plainPassword, _)) match {
+      case Right(true) => true
+      case _           => false
+    })
+  }
+
+  it should "syntax should generate same result" in {
+    val bcrypt = BCryptPasswordHasher()
+    val hash1: PasswordValidated[BCrypt] = bcrypt.hash(plainPassword)
+    val hash2: PasswordValidated[BCrypt] = plainPassword.hash(bcrypt)
+
+    assert(hash1 === hash2)
+  }
+
+  it should "return different results for different rounds" in {
+    val bcrypt1 = BCryptPasswordHasher(Rounds(DefaultBcryptRounds).asRight[Salt])
+    val bcrypt2 = BCryptPasswordHasher(Rounds(DefaultBcryptRounds+1).asRight[Salt])
+
+    assert(bcrypt1.hash(plainPassword) !== bcrypt2.hash(plainPassword))
+  }
 
 }
