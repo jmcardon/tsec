6c8f19ddb22731a7bbd06cdd1bae2ce9ecb428de
jose <cardona.josem@gmail.com>
2017-11-11 23:52:22 -0500

    change authenticator type param to A for readability

--- tsec-http4s/src/main/scala/tsec/authentication/AuthenticatorService.scala
+++ tsec-http4s/src/main/scala/tsec/authentication/AuthenticatorService.scala
@@ -12,13 +12,13 @@ import scala.concurrent.duration.FiniteDuration
   *
   * @tparam I The Identifier type
   * @tparam V The value type, i.e user, or possibly only partial information
-  * @tparam Authenticator the type of authenticator
+  * @tparam A the type of authenticator
   */
-abstract class AuthenticatorService[F[_]: Sync, I, V, Authenticator] {
+abstract class AuthenticatorService[F[_]: Sync, I, V, A] {
   val expiry: FiniteDuration
   val maxIdle: Option[FiniteDuration]
 
-  /** Attempt to retrieve the raw representation of an Authenticator
+  /** Attempt to retrieve the raw representation of an A
     * This is primarily useful when attempting to combine AuthenticatorService,
     * to be able to evaluate an endpoint with more than one token type.
     * or simply just to prod whether the request is malformed.
@@ -30,13 +30,13 @@ abstract class AuthenticatorService[F[_]: Sync, I, V, Authenticator] {
   /** Parse the raw representation from `extractRawOption`
     *
     */
-  def parseRaw(raw: String, request: Request[F]): OptionT[F, SecuredRequest[F, V, Authenticator]]
+  def parseRaw(raw: String, request: Request[F]): OptionT[F, SecuredRequest[F, V, A]]
 
   /** Return a secured request from a request, that carries our authenticator
     * @param request
     * @return
     */
-  def extractAndValidate(request: Request[F]): OptionT[F, SecuredRequest[F, V, Authenticator]] =
+  def extractAndValidate(request: Request[F]): OptionT[F, SecuredRequest[F, V, A]] =
     extractRawOption(request) match {
       case Some(raw) =>
         parseRaw(raw, request)
@@ -48,35 +48,35 @@ abstract class AuthenticatorService[F[_]: Sync, I, V, Authenticator] {
     * @param body
     * @return
     */
-  def create(body: I): OptionT[F, Authenticator]
+  def create(body: I): OptionT[F, A]
 
   /** Update the altered authenticator
     *
     * @param authenticator
     * @return
     */
-  def update(authenticator: Authenticator): OptionT[F, Authenticator]
+  def update(authenticator: A): OptionT[F, A]
 
   /** Delete an authenticator from a backing store, or invalidate it.
     *
     * @param authenticator
     * @return
     */
-  def discard(authenticator: Authenticator): OptionT[F, Authenticator]
+  def discard(authenticator: A): OptionT[F, A]
 
   /** Renew an authenticator: Reset it's expiry and whatnot.
     *
     * @param authenticator
     * @return
     */
-  def renew(authenticator: Authenticator): OptionT[F, Authenticator]
+  def renew(authenticator: A): OptionT[F, A]
 
   /** Refresh an authenticator: Primarily used for sliding window expiration
     *
     * @param authenticator
     * @return
     */
-  def refresh(authenticator: Authenticator): OptionT[F, Authenticator]
+  def refresh(authenticator: A): OptionT[F, A]
 
   /** Embed an authenticator directly into a response.
     * Particularly useful for adding an authenticator into unauthenticated actions
@@ -84,7 +84,7 @@ abstract class AuthenticatorService[F[_]: Sync, I, V, Authenticator] {
     * @param response
     * @return
     */
-  def embed(response: Response[F], authenticator: Authenticator): Response[F]
+  def embed(response: Response[F], authenticator: A): Response[F]
 
   /** Handles the embedding of the authenticator (if necessary) in the response,
     * and any other actions that should happen after a request related to authenticators
@@ -93,7 +93,7 @@ abstract class AuthenticatorService[F[_]: Sync, I, V, Authenticator] {
     * @param authenticator
     * @return
     */
-  def afterBlock(response: Response[F], authenticator: Authenticator): OptionT[F, Response[F]]
+  def afterBlock(response: Response[F], authenticator: A): OptionT[F, Response[F]]
 
 }
 
