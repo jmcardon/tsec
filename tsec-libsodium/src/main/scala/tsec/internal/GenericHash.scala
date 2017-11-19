7e6b58d69f95b357e77c9545db30d1c73545de3e
jose <cardona.josem@gmail.com>
2017-11-18 22:10:54 -0500

    generic hashing

--- tsec-libsodium/src/main/scala/tsec/internal/GenericHash.scala
+++ tsec-libsodium/src/main/scala/tsec/internal/GenericHash.scala
@@ -35,19 +35,19 @@ private[tsec] trait GenericHash {
 }
 
 private[tsec] trait GenericHashConstants {
-  val crypto_generichash_blake2b_BYTES_MIN = 16L
+  val crypto_generichash_blake2b_BYTES_MIN = 16
 
-  val crypto_generichash_blake2b_BYTES_MAX = 64L
+  val crypto_generichash_blake2b_BYTES_MAX = 64
 
   val crypto_generichash_blake2b_BYTES = 32
 
-  val crypto_generichash_blake2b_KEYBYTES_MIN = 16L
+  val crypto_generichash_blake2b_KEYBYTES_MIN = 16
 
-  val crypto_generichash_blake2b_KEYBYTES_MAX = 64L
+  val crypto_generichash_blake2b_KEYBYTES_MAX = 64
 
-  val crypto_generichash_blake2b_KEYBYTES = 32L
+  val crypto_generichash_blake2b_KEYBYTES = 32
 
-  val crypto_generichash_blake2b_SALTBYTES = 16L
+  val crypto_generichash_blake2b_SALTBYTES = 16
 
-  val crypto_generichash_blake2b_PERSONALBYTES = 16L
+  val crypto_generichash_blake2b_PERSONALBYTES = 16
 }
---
+++ tsec-libsodium/src/main/scala/tsec/libsodium/hashing/package.scala
@@ -0,0 +1,92 @@
+package tsec.libsodium
+
+import cats.effect.Sync
+import cats.evidence.Is
+import tsec.ScalaSodium
+import tsec.ScalaSodium.{NullLongBytes, NullLongLong}
+import tsec.common.TaggedByteArray
+import tsec.libsodium.hashing.BlakeKey$$
+
+package object hashing {
+
+  private[tsec] val Blake2b$$ : TaggedByteArray = new TaggedByteArray {
+    type I = Array[Byte]
+    val is = Is.refl[I]
+  }
+
+  type Blake2b = Blake2b$$.I
+
+  private[tsec] val BlakeKey$$ : TaggedByteArray = new TaggedByteArray {
+    type I = Array[Byte]
+    val is = Is.refl[I]
+  }
+
+  type BlakeKey = BlakeKey$$.I
+
+  object BlakeKey {
+    def apply(bytes: Array[Byte]): BlakeKey   = is.flip.coerce(bytes)
+    @inline def is: Is[BlakeKey, Array[Byte]] = BlakeKey$$.is
+  }
+
+  object Blake2b {
+    val MinKeyLen     = ScalaSodium.crypto_generichash_blake2b_KEYBYTES_MIN
+    val DefaultKeyLen = ScalaSodium.crypto_generichash_blake2b_KEYBYTES
+    val MaxKeyLen     = ScalaSodium.crypto_generichash_blake2b_KEYBYTES_MAX
+
+    val MinHashLen     = ScalaSodium.crypto_generichash_blake2b_BYTES_MIN
+    val DefaultHashLen = ScalaSodium.crypto_generichash_blake2b_BYTES
+    val MaxHashLen     = ScalaSodium.crypto_generichash_blake2b_BYTES_MAX
+
+    def apply(bytes: Array[Byte]): Blake2b   = is.flip.coerce(bytes)
+    @inline def is: Is[Blake2b, Array[Byte]] = Blake2b$$.is
+
+    def generateKey[F[_]](implicit F: Sync[F], S: ScalaSodium): F[BlakeKey] = F.delay {
+      BlakeKey(ScalaSodium.randomBytesUnsafe(DefaultKeyLen))
+    }
+
+    def generateMinKey[F[_]](implicit F: Sync[F], S: ScalaSodium): F[BlakeKey] = F.delay {
+      BlakeKey(ScalaSodium.randomBytesUnsafe(MinKeyLen))
+    }
+
+    def generateMaxKey[F[_]](implicit F: Sync[F], S: ScalaSodium): F[BlakeKey] = F.delay {
+      BlakeKey(ScalaSodium.randomBytesUnsafe(MaxKeyLen))
+    }
+
+    def generateKeyVarLen[F[_]](len: Int)(implicit F: Sync[F], S: ScalaSodium): F[BlakeKey] = F.delay {
+      val outLen = math.max(MinKeyLen, math.min(MaxKeyLen, len))
+      BlakeKey(ScalaSodium.randomBytesUnsafe(outLen))
+    }
+
+    def hash[F[_]](in: Array[Byte])(implicit F: Sync[F], S: ScalaSodium): F[Blake2b] = F.delay {
+      val out = new Array[Byte](DefaultHashLen)
+      S.crypto_generichash(out, DefaultHashLen, in, in.length, NullLongBytes, 0)
+      Blake2b(out)
+    }
+
+    def hashVarLen[F[_]](in: Array[Byte], len: Int = DefaultHashLen)(implicit F: Sync[F], S: ScalaSodium): F[Blake2b] =
+      F.delay {
+        val outLen = math.max(MinHashLen, math.min(MaxHashLen, len))
+        val out    = new Array[Byte](outLen)
+        S.crypto_generichash(out, outLen, in, in.length, NullLongBytes, 0)
+        Blake2b(out)
+      }
+
+    def hashKeyed[F[_]](in: Array[Byte], key: BlakeKey)(implicit F: Sync[F], S: ScalaSodium): F[Blake2b] = F.delay {
+      val out = new Array[Byte](DefaultHashLen)
+      S.crypto_generichash(out, DefaultHashLen, in, in.length, key, key.length)
+      Blake2b(out)
+    }
+
+    def hashKeyedVarLen[F[_]](in: Array[Byte], key: BlakeKey, len: Int = DefaultHashLen)(
+        implicit F: Sync[F],
+        S: ScalaSodium
+    ): F[Blake2b] = F.delay {
+      val outLen = math.max(MinHashLen, math.min(MaxHashLen, len))
+      val out    = new Array[Byte](outLen)
+      S.crypto_generichash(out, outLen, in, in.length, key, key.length)
+      Blake2b(out)
+    }
+
+  }
+
+}
