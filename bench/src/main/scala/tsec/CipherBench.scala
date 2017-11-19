885d7dcad9d91159bb515a5654e22a6311c7af9e
jose <cardona.josem@gmail.com>
2017-11-19 04:02:57 -0500

    refine aes benchmark

--- bench/src/main/scala/tsec/CipherBench.scala
+++ bench/src/main/scala/tsec/CipherBench.scala
@@ -1,6 +1,8 @@
 package tsec
 
 import java.util.concurrent.TimeUnit
+import javax.crypto
+import javax.crypto.Cipher
 
 import org.openjdk.jmh.annotations._
 import cats.effect.IO
@@ -12,7 +14,6 @@ import tsec.libsodium._
 import tsec.libsodium.cipher._
 import tsec.common._
 import tsec.libsodium.cipher.aead.AES256GCM
-
 import scala.util.Random
 
 @State(Scope.Thread)
@@ -20,20 +21,33 @@ import scala.util.Random
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 class CipherBench {
 
-  implicit lazy val sodium                    = ScalaSodium.getSodiumUnsafe
-  lazy val chachaKey                          = XChacha20Poly1305.generateKeyUnsafe
-  lazy val sodiumAESKey: SodiumKey[AES256GCM] = AES256GCM.generateKeyUnsafe
-  lazy val jcaAESKey: SecretKey[AES256]       = AES256.generateKeyUnsafe()
-  lazy val jcaAES                             = JCAAEADPure[IO, AES256, GCM, NoPadding]().unsafeRunSync()
-  lazy val rand                               = new Random()
-  lazy val longPlaintext                      = PlainText(Array.fill[Char](5000)(Random.nextInt(127).toChar).mkString.utf8Bytes)
+  /** Our libsodium setup **/
+  implicit lazy val sodium: ScalaSodium            = ScalaSodium.getSodiumUnsafe
+  lazy val chachaKey: SodiumKey[XChacha20Poly1305] = XChacha20Poly1305.generateKeyUnsafe
+  lazy val sodiumAESKey: SodiumKey[AES256GCM]      = AES256GCM.generateKeyUnsafe
+
+  /** AES using tsec classes **/
+  lazy val jcaAESKey: SecretKey[AES256]                    = AES256.generateKeyUnsafe()
+  lazy val jcaAES: JCAAEADPure[IO, AES256, GCM, NoPadding] = JCAAEADPure[IO, AES256, GCM, NoPadding]().unsafeRunSync()
+
+  /** Our AES using the JCA raw classes. Note: We reuse cipher the instance for speed, but it's not thread safe **/
+  lazy val jcaRAWKey: crypto.SecretKey = SecretKey.toJavaKey(AES256.generateKeyUnsafe())
+  lazy val jcaRAWInstance: Cipher      = Cipher.getInstance("AES/GCM/NoPadding")
+
+  /** Our random plaintext **/
+  lazy val longPlaintext: PlainText = PlainText(Array.fill[Char](5000)(Random.nextInt(127).toChar).mkString.utf8Bytes)
+
+  @Benchmark
+  def testJCARawSideEffecting(): Unit = {
+    jcaRAWInstance.init(Cipher.ENCRYPT_MODE, jcaRAWKey)
+    jcaRAWInstance.doFinal(longPlaintext.content)
+  }
 
   /** We test each io action
     * to view the related overhead, but we do not care about sequencing them
-    *
     */
   @Benchmark
-  def testJCA(): Unit =
+  def testTSecJCA(): Unit =
     jcaAES
       .encrypt(longPlaintext, jcaAESKey)
       .unsafeRunSync()
