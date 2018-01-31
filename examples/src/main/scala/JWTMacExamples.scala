01eb5aa8d20880c59f78b5b6816f3bd2ab4801b6
jose <cardona.josem@gmail.com>
2018-01-31 02:07:18 -0500

    Fix jwt sig

--- examples/src/main/scala/JWTMacExamples.scala
+++ examples/src/main/scala/JWTMacExamples.scala
@@ -1,3 +1,5 @@
+import java.time.Instant
+
 object JWTMacExamples {
 
   import tsec.jwt._
@@ -7,14 +9,12 @@ object JWTMacExamples {
   import cats.syntax.all._
   import cats.effect.Sync
 
-  /** To create custom claims: */
-  val claims = JWTClaims.build(expiration = Some(10.minutes))
-
   /** You can interpret into any target Monad with an instance of Sync[F] using JwtMac */
   def jwtMonadic[F[_]: Sync]: F[JWTMac[HMACSHA256]] =
     for {
-      key <- HMACSHA256.generateLift[F]
-      jwt <- JWTMac.build[F, HMACSHA256](claims, key) //You can sign and build a jwt object directly
+      key    <- HMACSHA256.generateLift[F]
+      claims <- JWTClaims.withDuration[F](expiration = Some(10.minutes))
+      jwt    <- JWTMac.build[F, HMACSHA256](claims, key) //You can sign and build a jwt object directly
       verifiedFromObj <- JWTMac
         .verifyFromInstance[F, HMACSHA256](jwt, key) //You can verify the jwt straight from an object
       stringjwt  <- JWTMac.buildToString[F, HMACSHA256](claims, key)       //Or build it straight to string
@@ -23,11 +23,13 @@ object JWTMacExamples {
     } yield parsed
 
   /** Using impure either interpreters */
+  val impureClaims = JWTClaims(expiration = Some(Instant.now.plusSeconds(10.minutes.toSeconds)))
+
   val jwt: Either[Throwable, JWTMac[HMACSHA256]] = for {
     key             <- HMACSHA256.generateKey()
-    jwt             <- JWTMacImpure.build[HMACSHA256](claims, key) //You can sign and build a jwt object directly
+    jwt             <- JWTMacImpure.build[HMACSHA256](impureClaims, key) //You can sign and build a jwt object directly
     verifiedFromObj <- JWTMacImpure.verifyFromInstance[HMACSHA256](jwt, key)
-    stringjwt       <- JWTMacImpure.buildToString[HMACSHA256](claims, key) //Or build it straight to string
+    stringjwt       <- JWTMacImpure.buildToString[HMACSHA256](impureClaims, key) //Or build it straight to string
     isverified      <- JWTMacImpure.verifyFromString[HMACSHA256](stringjwt, key) //You can verify straight from a string
     parsed          <- JWTMacImpure.verifyAndParse[HMACSHA256](stringjwt, key) //Or verify and return the actual instance
   } yield parsed
