35d5e1b4e0762e5dedd0125d44b60842aea7fa6f
jose <cardona.josem@gmail.com>
2018-03-22 03:41:36 -0400

    add user aware service

--- tsec-http4s/src/main/scala/tsec/authentication/SecuredRequestHandler.scala
+++ tsec-http4s/src/main/scala/tsec/authentication/SecuredRequestHandler.scala
@@ -58,6 +58,18 @@ sealed abstract class SecuredRequestHandler[F[_], Identity, User, Auth](
       }
   }
 
+  def liftUserAware(
+    service: UserAwareService[User, Auth, F]
+  ): HttpService[F] = {
+    val middleware = UserAwareService.extract(Kleisli(authenticator.extractAndValidate))
+
+    middleware(service)
+      .handleErrorWith { e =>
+        SecuredRequestHandler.logger.error(e)("Caught unhandled exception in authenticated service")
+        Kleisli.liftF(OptionT.pure(cachedUnauthorized))
+      }
+  }
+
   /** Create an Authorized Service **/
   @deprecated("Use TSecAuthService + liftService", "0.0.1-M10")
   def authorized(authorization: Authorization[F, User, Auth])(
