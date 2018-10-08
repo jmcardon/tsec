046df78df4d6c70ab6a4a3628070944dd46b33d9
Alex Henning Johannessen <ahjohannessen@gmail.com>
2018-10-08 12:16:49 +0100

    deps: upgrade fs2 & http4s
     - upgrade dependencies for fs2 & http4s

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,32 +8,32 @@ object Dependencies {
     val bouncyCastleV = "1.60"
     val sCryptV       = "1.4.0"
     val scalaTestV    = "3.0.5"
-    val http4sV       = "0.19.0-M3"
+    val http4sV       = "0.19.0-M4"
     val scalacheckV   = "1.14.0"
     val commonsCodecV = "1.11"
-    val fs2Version    = "1.0.0-RC2"
+    val fs2Version    = "1.0.0"
     val log4sV        = "1.6.1"
     val gitHub4s      = "0.18.8"
   }
 
   object Libraries {
-    val fs2                = "co.fs2"             %% "fs2-core"             % Versions.fs2Version
-    val fs2IO              = "co.fs2"             %% "fs2-io"               % Versions.fs2Version
-    val cats               = "org.typelevel"      %% "cats-core"            % Versions.catsV
-    val sCrypt             = "com.lambdaworks"    % "scrypt"                % Versions.sCryptV
-    val scalaTest          = "org.scalatest"      %% "scalatest"            % Versions.scalaTestV % "test"
-    val BC                 = "org.bouncycastle"   % "bcprov-jdk15on"        % Versions.bouncyCastleV
-    val circeCore          = "io.circe"           %% "circe-core"           % Versions.circeV
-    val circeGeneric       = "io.circe"           %% "circe-generic"        % Versions.circeV
-    val circeGenericExtras = "io.circe"           %% "circe-generic-extras" % Versions.circeV
-    val circeParser        = "io.circe"           %% "circe-parser"         % Versions.circeV
-    val http4sdsl          = "org.http4s"         %% "http4s-dsl"           % Versions.http4sV
-    val http4sServer       = "org.http4s"         %% "http4s-server"        % Versions.http4sV
-    val http4sCirce        = "org.http4s"         %% "http4s-circe"         % Versions.http4sV % "test"
-    val scalaCheck         = "org.scalacheck"     %% "scalacheck"           % Versions.scalacheckV % "test"
-    val commonsCodec       = "commons-codec"      % "commons-codec"         % Versions.commonsCodecV
-    val log4s              = "org.log4s"          %% "log4s"                % Versions.log4sV
-    val gitHub4s           = "com.47deg"          %% "github4s"             % Versions.gitHub4s
+    val fs2                = "co.fs2"           %% "fs2-core"             % Versions.fs2Version
+    val fs2IO              = "co.fs2"           %% "fs2-io"               % Versions.fs2Version
+    val cats               = "org.typelevel"    %% "cats-core"            % Versions.catsV
+    val sCrypt             = "com.lambdaworks"  % "scrypt"                % Versions.sCryptV
+    val scalaTest          = "org.scalatest"    %% "scalatest"            % Versions.scalaTestV % "test"
+    val BC                 = "org.bouncycastle" % "bcprov-jdk15on"        % Versions.bouncyCastleV
+    val circeCore          = "io.circe"         %% "circe-core"           % Versions.circeV
+    val circeGeneric       = "io.circe"         %% "circe-generic"        % Versions.circeV
+    val circeGenericExtras = "io.circe"         %% "circe-generic-extras" % Versions.circeV
+    val circeParser        = "io.circe"         %% "circe-parser"         % Versions.circeV
+    val http4sdsl          = "org.http4s"       %% "http4s-dsl"           % Versions.http4sV
+    val http4sServer       = "org.http4s"       %% "http4s-server"        % Versions.http4sV
+    val http4sCirce        = "org.http4s"       %% "http4s-circe"         % Versions.http4sV % "test"
+    val scalaCheck         = "org.scalacheck"   %% "scalacheck"           % Versions.scalacheckV % "test"
+    val commonsCodec       = "commons-codec"    % "commons-codec"         % Versions.commonsCodecV
+    val log4s              = "org.log4s"        %% "log4s"                % Versions.log4sV
+    val gitHub4s           = "com.47deg"        %% "github4s"             % Versions.gitHub4s
   }
 
 }
