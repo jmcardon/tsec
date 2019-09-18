a8d91b639e21b61edf651acf57c1b32a7e0bd21e
Scala Steward <me@scala-steward.org>
2019-09-19 01:53:13 +0200

    Update scalacheck to 1.14.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val scalaTestV    = "3.1.0-SNAP13"
     val scalaTestPlusV= "1.0.0-SNAP8"
     val http4sV       = "0.21.0-M1"
-    val scalacheckV   = "1.14.0"
+    val scalacheckV   = "1.14.1"
     val commonsCodecV = "1.12"
     val fs2Version    = "1.1.0-M1"
     val log4sV        = "1.8.2"
