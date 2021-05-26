8b96de202d85599e4eb30c005c03269b2ed020be
Scala Steward <me@scala-steward.org>
2021-05-26 12:49:41 +0200

    Update circe-core, circe-generic, ... to 0.14.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val circeV        = "0.14.0-M7"   //https://github.com/circe/circe/releases
+    val circeV        = "0.14.0"   //https://github.com/circe/circe/releases
     val catsV         = "2.6.1"        //https://github.com/typelevel/cats/releases
     val bouncyCastleV = "1.68"         //https://github.com/bcgit/bc-java/releases
     val sCryptV       = "1.4.0"        //https://github.com/wg/scrypt/releases
