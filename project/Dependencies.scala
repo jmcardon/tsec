a57e0f0b415db45c0d7109e8998a812fb5d2961a
Scala Steward <me@scala-steward.org>
2021-09-03 02:43:59 +0200

    Update http4s-circe, http4s-dsl, ... to 0.23.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val sCryptV       = "1.4.0"        //https://github.com/wg/scrypt/releases
     val scalaTestV    = "3.2.9"      //https://github.com/scalatest/scalatest/releases
     val scalaTestPlusV= "3.2.9.0"  //https://github.com/scalatest/scalatestplus-scalacheck
-    val http4sV       = "0.23.1"   //https://github.com/http4s/http4s/releases
+    val http4sV       = "0.23.3"   //https://github.com/http4s/http4s/releases
     val scalacheckV   = "1.15.4"       //https://github.com/typelevel/scalacheck/releases
     val commonsCodecV = "1.15"         //https://github.com/apache/commons-codec/releases
     val fs2Version    = "3.0.3"        //https://github.com/functional-streams-for-scala/fs2/releases
